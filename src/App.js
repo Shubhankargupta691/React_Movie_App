d401f7cfff1b4d78fa43efd6d170f8ffaed71308
Shubhankargupta691 <shubhankargupta691@gmail.com>
2023-09-01 20:34:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchSvg from './search.svg'
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const API_URL = 'http://www.omdbapi.com?apiKey=fd92f3b4';
+
+
+
+
+const App = () => {
+
+  const  [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  // @ts-check SEARCH MOVIES
+  const searchMovies = async(title) => {
+  
+    try{
+
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        console.log(data);
+        setMovies(data.Search);
+    } 
+    catch(error){
+         console.error('Error fetching data:', error);
+    } 
+
+}
+
+// @ts-check USE EFFECT
+  useEffect(() => {
+    searchMovies('avengers');
+  }, []);
+
+
+  const handleSearch = ()=> {
+
+   setSearchTerm(`${searchTerm}`);
+  };
+  
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+ 
+
+// @ts-check RETURN
+  return(
+    <div className='app'> 
+        <h1>UHD Movies</h1>
+      
+    <div className='search'>
+
+      <input
+        placeholder='Search for movies'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onKeyPress={handleKeyPress}
+      />
+    
+      <img 
+        src={SearchSvg}
+        alt='search'
+        onClick={() => searchMovies(searchTerm)}
+      />
+    </div>
+
+    {
+        movies?.length > 0? (
+          <div className='container'>
+           {
+            movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))
+           }
+          </div>
+        ):(
+          <div className='empty'>
+            <h1>No movies found</h1>  
+          </div>
+           )
+    }
+
+    
+
     </div>
   );
 }
